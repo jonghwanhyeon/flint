801803d4c4f3ee1940fadb8422228b7988cc84f6
Jonghwan Hyeon <hyeon0145@gmail.com>
2020-11-03 01:40:41 +0000

    Add ModelCheckPoint

--- train.py
+++ train.py
@@ -2,11 +2,14 @@ import os
 from functools import partial
 
 import torch
-from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
 
 from flint.datamodule import SkeletonDataModule
 from flint.experiments.skeleton import Skeleton
+
+identifier = 'skeleton'
 
 if __name__ == '__main__':
     datamodule = SkeletonDataModule(
@@ -16,16 +19,21 @@ if __name__ == '__main__':
             'test': 24,
         },
     )
-
     experiment = Skeleton(
         optimizer=partial(torch.optim.SGD, lr=0.001, momentum=0.9),
         lr_scheduler=partial(torch.optim.lr_scheduler.StepLR, step_size=128, gamma=0.98),
     )
 
-    logger = TensorBoardLogger(save_dir=os.getcwd(), name='logs/skeleton')
     trainer = Trainer(
         gpus=[0], 
         num_sanity_val_steps=0,
-        logger=logger,
+        logger=TensorBoardLogger(save_dir=os.getcwd(), name=f'logs/{identifier}'),
+        callbacks=[
+            ModelCheckpoint(
+                monitor='validation_loss',
+                dirpath=f'checkpoints/{identifier}',
+                filename='{epoch}-{step}-{val_loss:.4f}',
+            ),
+        ]
     )
     trainer.fit(experiment, datamodule)
